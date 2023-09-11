f5748930627463bd0bd6929e48d2b2db5b26ed24
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-09-11 00:00:33 -0700

    add the header file

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <sys/wait.h>
+
+int _putchar(char c);
+
+
+#endif
