0cbefe4ea7de31654db3d0884c11a979fef7bef0
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-09-14 01:14:05 -0700

    add more helper fonctions

--- shell.h
+++ shell.h
@@ -18,6 +18,7 @@ void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2, int n);
-
+char *_strdup(char *str);
+char *_strchr(const char *str, int c);
 
 #endif
