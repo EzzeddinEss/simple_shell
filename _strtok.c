8449c399ad4cc21aaa341dd6eae819449bc07f38
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-09-14 01:29:24 -0700

    fix betty

--- _strtok.c
+++ _strtok.c
@@ -2,7 +2,7 @@
 
 /**
  * _strtok: tokenize a given string.
- * @line: the line to be modified. 
+ * @line: the line to be modified.
  * @stk: an array of strings
  * Return: a pointer to the first token that found
 */
