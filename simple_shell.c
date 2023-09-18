9fa32c2caa8b4e64ff0017c8d2f81f1680b9fbc0
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-09-18 01:42:46 -0700

    hope it works

--- simple_shell.c
+++ simple_shell.c
@@ -9,14 +9,26 @@
 int main(int argc, char *argv[])
 {
 	char *stk[10], *av[] = {NULL}, *line = NULL;
-
+	int space;
+	
 	if (argc != 1)
 	{
 		return (0);
 	}
-	while(1)
+	while (1)
 	{
 		_puts("$ ");
+		line = _getline(line);
+		space = test_white_space(line);
+		if (space == 1)
+		{
+			stk[0] = "\n";
+			continue;
+		}
+		_strtok(line, stk);
+		_fork2(stk, av, argv);
 	}
+	free(line);
 	return (0);
 }
+
